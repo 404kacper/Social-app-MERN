c623f1b8ae66ae598555d358d2712c9f47a8746c
Kacper <kacper.sionkowsk@gmail.com>
2019-07-24 13:12:38 +0200

    More front end job, done welcome, login, register, profile, dashboard, add-edu, add-exp routes

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Experience = ({ experience }) => {
+  return <div />;
+};
+
+Experience.propTypes = {};
+
+export default Experience;
