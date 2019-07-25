7e82e4b195d49c3b66cf056b8b49f2683640105a
Kacper <kacper.sionkowsk@gmail.com>
2019-07-25 16:22:54 +0200

    Fixed typo

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-// Server static assets in production
-if (process.nextTick.NODE_ENV === 'production') {
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
